ca34c4e4f2feab0f5e36b059b00f36bb6f654bfa
0Leo0h <ledahepa@gmail.com>
2022-11-10 11:17:26 -0500

    Auditoria

--- conexion.php
+++ conexion.php
@@ -26,6 +26,30 @@ $result = mysqli_query($conexion, $sql);
 if ($row = mysqli_fetch_array($result)) {
     if (password_verify($pass, $row['password'])) {
         echo 'Password is valid!';
+        
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+        $ip =  get_client_ip();
+         
+$sql2 = "INSERT INTO auditoria (user, ip) VALUES ('$usuario', '$ip')";
+$result2 = mysqli_query($conexion, $sql2);
+
         if ($row['rol'] == "admin") {
             session_start();
             $_SESSION['rol'] = $row['rol'];
